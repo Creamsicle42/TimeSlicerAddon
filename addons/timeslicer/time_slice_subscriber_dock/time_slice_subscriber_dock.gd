249bb34ee0624b79118d75d06dc5a36a398b0fea
Annabelle Lewis <defuego424242@gmail.com>
2023-07-13 10:45:47 -0700

    Added code for subscribing nodes
    Added the code to subscribe nodes to time slices

--- addons/timeslicer/time_slice_subscriber_dock/time_slice_subscriber_dock.gd
+++ addons/timeslicer/time_slice_subscriber_dock/time_slice_subscriber_dock.gd
@@ -6,10 +6,39 @@ class_name TimeSliceSubscriberDock extends Control
 @onready var time_slice_name_field: LineEdit = %TimeSliceNameField
 
 
+var selected_node : Node
+
 
 func _on_editor_selection_changed(new_selection: EditorSelection) -> void:
-	print_debug(new_selection.get_selected_nodes().size())
+	if new_selection.get_selected_nodes().size() != 1:
+		selected_node = null
+	else:
+		selected_node = new_selection.get_selected_nodes()[0]
 
 
 func _on_subscribe_time_slice_button_pressed() -> void:
-	pass # Replace with function body.
+	# Test if selected node exists
+	if selected_node == null: return
+	
+	# Get data from interface
+	var method_name = method_name_field.text
+	var time_slice_name = time_slice_name_field.text
+	
+	# Get existing node metadata
+	var node_metadata:Dictionary = selected_node.get_meta("time_slice_data", {})
+	
+	# Test to make sure metadata is valid
+	if not node_metadata is Dictionary: 
+		push_error("Node Metadata is invalid")
+		return
+	
+	# Make sure node does not already have this method subscribed
+	if node_metadata.get(time_slice_name, []).has(method_name):
+		return
+	
+	# Add method to metadata
+	node_metadata.get(time_slice_name, []).append(method_name)
+	
+	# Add node metadata and group if needed
+	selected_node.set_meta("time_slice_data", node_metadata)
+	if not selected_node.is_in_group("time_slice_subscribed"): selected_node.add_to_group("time_slice_subscribed")
