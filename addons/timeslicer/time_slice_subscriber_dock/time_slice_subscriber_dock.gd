5626730542f5d7732d111612ff10c905a3f53519
Annabelle Lewis <defuego424242@gmail.com>
2023-07-13 11:03:31 -0700

    Fixed some errors with time slice subscribing

--- addons/timeslicer/time_slice_subscriber_dock/time_slice_subscriber_dock.gd
+++ addons/timeslicer/time_slice_subscriber_dock/time_slice_subscriber_dock.gd
@@ -4,6 +4,7 @@ class_name TimeSliceSubscriberDock extends Control
 
 @onready var method_name_field: LineEdit = %MethodNameField
 @onready var time_slice_name_field: LineEdit = %TimeSliceNameField
+@onready var subscribe_time_slice_button: Button = $VBoxContainer/SubscribeTimeSliceButton
 
 
 var selected_node : Node
@@ -14,6 +15,19 @@ func _on_editor_selection_changed(new_selection: EditorSelection) -> void:
 		selected_node = null
 	else:
 		selected_node = new_selection.get_selected_nodes()[0]
+	update_pannel_visuals()
+
+
+func update_pannel_visuals() -> void:
+	if selected_node == null:
+		method_name_field.editable = false
+		time_slice_name_field.editable = false
+		subscribe_time_slice_button.disabled = true
+	else:
+		method_name_field.editable = true
+		time_slice_name_field.editable = true
+		subscribe_time_slice_button.disabled = false
+		print_debug(selected_node.get_meta("time_slice_data", {}))
 
 
 func _on_subscribe_time_slice_button_pressed() -> void:
@@ -34,10 +48,15 @@ func _on_subscribe_time_slice_button_pressed() -> void:
 	
 	# Make sure node does not already have this method subscribed
 	if node_metadata.get(time_slice_name, []).has(method_name):
+		print_debug("Node already has method %s subscribed to %s" % [method_name, time_slice_name])
 		return
 	
 	# Add method to metadata
-	node_metadata.get(time_slice_name, []).append(method_name)
+	var slice_data = []
+	if node_metadata.has(time_slice_name):
+		slice_data = node_metadata[time_slice_name]
+	slice_data.append(method_name)
+	node_metadata[time_slice_name] = slice_data
 	
 	# Add node metadata and group if needed
 	selected_node.set_meta("time_slice_data", node_metadata)
